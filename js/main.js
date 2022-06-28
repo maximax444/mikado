d2dabcdaa0fd9aef05708cbea788499da39bc990
Max <max.kuzn.msx@gmail.com>
2022-06-28 15:40:34 +0300

    add home-page animation

--- js/main.js
+++ js/main.js
@@ -10,7 +10,275 @@ $('.contacts__cities-title').on('click', function () {
     }
 
 });
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+const length_width = 1;
+let way = "top";
+let way2 = "top";
+let way3 = "top";
+let way4 = "top";
+let wayPart = 1;
+let snakeCoord = [];
+let snakeCoord2 = [];
+let snakeCoord3 = [];
+let snakeCoord4 = [];
+let snakeCoord5 = [];
+snakeCoord.push({ x: 598, y: 149, z: 1 })
+for (let i = 0; i < 200; i++) {
+    snakeCoord.push({ x: 598, y: 150 + i, z: 0 })
+}
+snakeCoord.push({ x: 598, y: 350, z: 1 })
+for (let j = 0; j < 200; j++) {
+    snakeCoord2.push({ x: 547, y: 100 + j, z: 0 })
+}
+for (let i = 0; i < 200; i++) {
+    snakeCoord3.push({ x: 497, y: 150 + i, z: 0 })
+}
+for (let j = 0; j < 200; j++) {
+    snakeCoord4.push({ x: 447, y: 100 + j, z: 0 })
+}
+for (let j = 0; j < 340; j++) {
+    snakeCoord5.push({ x: 395, y: 10 + j, z: 0 })
+}
+function drawSnakePart(snakePart) {
+    ctx.beginPath();
+    ctx.fillStyle = "#32A5EB";
+    if (snakePart.z == 1) {
+        ctx.fillRect(snakePart.x, snakePart.y, 1, 1);
+        ctx.fillRect(snakePart.x - 1, snakePart.y, 1, 1);
+        ctx.fillRect(snakePart.x + 1, snakePart.y, 1, 1);
+        ctx.fillRect(snakePart.x, snakePart.y - 1, 1, 1);
+        ctx.fillRect(snakePart.x, snakePart.y + 1, 1, 1);
+        ctx.fillRect(snakePart.x, snakePart.y, 1, 1);
+        ctx.fillRect(snakePart.x - 2, snakePart.y, 1, 1);
+        ctx.fillRect(snakePart.x + 2, snakePart.y, 1, 1);
+        ctx.fillRect(snakePart.x, snakePart.y - 2, 1, 1);
+        ctx.fillRect(snakePart.x, snakePart.y + 2, 1, 1);
+        ctx.fillRect(snakePart.x - 1, snakePart.y - 1, 1, 1);
+        ctx.fillRect(snakePart.x + 1, snakePart.y + 1, 1, 1);
+        ctx.fillRect(snakePart.x - 1, snakePart.y + 1, 1, 1);
+        ctx.fillRect(snakePart.x + 1, snakePart.y - 1, 1, 1);
+    } else {
+        ctx.fillRect(snakePart.x, snakePart.y, length_width, length_width);
+    }
 
+    ctx.closePath();
+}
+
+function drawSnake() {
+    snakeCoord.forEach(drawSnakePart);
+    snakeCoord2.forEach(drawSnakePart);
+    snakeCoord3.forEach(drawSnakePart);
+    snakeCoord4.forEach(drawSnakePart);
+    snakeCoord5.forEach(drawSnakePart);
+}
+
+function moveSnake(dx, dy) {
+    const head = {
+        x: snakeCoord[0].x + dx,
+        y: snakeCoord[0].y + dy,
+        z: 1
+    };
+    snakeCoord[1].z = 0;
+    snakeCoord[snakeCoord.length - 2].z = 1;
+    snakeCoord.unshift(head);
+    snakeCoord.pop();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawSnake();
+    setTimeout(function () {
+        if (head.y < 1 && way == "top") {
+            moveSnake(-length_width, 0);
+            way = "left"
+        } else if (head.x < 560 && way == "left") {
+            moveSnake(0, length_width);
+            way = "down"
+        } else if (head.y > 317 && way == "down") {
+            moveSnake(length_width, 0);
+            way = "right"
+        } else if (head.x > 598 && way == "right") {
+            moveSnake(0, -length_width);
+            way = "top"
+        } else {
+            moveSnake(dx, dy);
+        }
+
+    }, 1);
+}
+function moveSnake2(dx, dy) {
+    const head = {
+        x: snakeCoord2[0].x + dx,
+        y: snakeCoord2[0].y + dy,
+        z: 1
+    };
+    snakeCoord2[1].z = 0;
+    snakeCoord2[snakeCoord2.length - 2].z = 1;
+    snakeCoord2.unshift(head);
+    snakeCoord2.pop();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawSnake();
+    setTimeout(function () {
+        if (head.y < 1 && way2 == "top") {
+            moveSnake2(-length_width, 0);
+            way2 = "left"
+        } else if (head.x < 520 && way2 == "left") {
+            moveSnake2(0, length_width);
+            way2 = "down"
+        } else if (head.y > 317 && way2 == "down") {
+            moveSnake2(length_width, 0);
+            way2 = "right"
+        } else if (head.x > 547 && way2 == "right") {
+            moveSnake2(0, -length_width);
+            way2 = "top"
+        } else {
+            moveSnake2(dx, dy);
+        }
+
+    }, 20);
+}
+function moveSnake3(dx, dy) {
+    const head = {
+        x: snakeCoord3[0].x + dx,
+        y: snakeCoord3[0].y + dy,
+        z: 1
+    };
+    snakeCoord3[1].z = 0;
+    snakeCoord3[snakeCoord3.length - 2].z = 1;
+    snakeCoord3.unshift(head);
+    snakeCoord3.pop();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawSnake();
+    setTimeout(function () {
+        if (head.y < 1 && way3 == "top") {
+            moveSnake3(-length_width, 0);
+            way3 = "left"
+        } else if (head.x < 480 && way3 == "left") {
+            moveSnake3(0, length_width);
+            way3 = "down"
+        } else if (head.y > 317 && way3 == "down") {
+            moveSnake3(length_width, 0);
+            way3 = "right"
+        } else if (head.x > 497 && way3 == "right") {
+            moveSnake3(0, -length_width);
+            way3 = "top"
+        } else {
+            moveSnake3(dx, dy);
+        }
+
+    }, 10);
+}
+
+function moveSnake4(dx, dy) {
+    const head = {
+        x: snakeCoord4[0].x + dx,
+        y: snakeCoord4[0].y + dy,
+        z: 1
+    };
+    snakeCoord4[1].z = 0;
+    snakeCoord4[snakeCoord4.length - 2].z = 1;
+    snakeCoord4.unshift(head);
+    snakeCoord4.pop();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawSnake();
+    setTimeout(function () {
+        if (head.y < 1 && way4 == "top") {
+            moveSnake4(-length_width, 0);
+            way4 = "left"
+        } else if (head.x < 438 && way4 == "left") {
+            moveSnake4(0, length_width);
+            way4 = "down"
+        } else if (head.y > 317 && way4 == "down") {
+            moveSnake4(length_width, 0);
+            way4 = "right"
+        } else if (head.x >= 447 && way4 == "right") {
+            moveSnake4(0, -length_width);
+            way4 = "top"
+        } else {
+            moveSnake4(dx, dy);
+        }
+
+    }, 10);
+}
+function moveSnake5(dx, dy) {
+    const head = {
+        x: snakeCoord5[0].x + dx,
+        y: snakeCoord5[0].y + dy,
+        z: 1
+    };
+    snakeCoord5[1].z = 0;
+    snakeCoord5[snakeCoord5.length - 2].z = 1;
+    snakeCoord5.unshift(head);
+    snakeCoord5.pop();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawSnake();
+    setTimeout(function () {
+        if (head.y < 1 && wayPart == 1) {
+            moveSnake5(-length_width, 0);
+            wayPart = 2
+        } else if (head.x < 268 && wayPart == 2) {
+            moveSnake5(-0.37, 1);
+            wayPart = 3
+        } else if (head.x < 199 && wayPart == 3) {
+            moveSnake5(-0.37, -1);
+            wayPart = 4
+        } else if (head.y < 1 && wayPart == 4) {
+            moveSnake5(-length_width, 0);
+            wayPart = 5
+        } else if (head.x < 1 && wayPart == 5) {
+            moveSnake5(0, 1);
+            wayPart = 6
+        } else if (head.y > 317 && wayPart == 6) {
+            moveSnake5(length_width, 0);
+            wayPart = 7
+        } else if (head.x > 90 && wayPart == 7) {
+            moveSnake5(0, -1);
+            wayPart = 8
+        } else if (head.y < 125 && wayPart == 8) {
+            moveSnake5(0.37, 1);
+            wayPart = 9
+        } else if (head.y > 317 && wayPart == 9) {
+            moveSnake5(length_width, 0);
+            wayPart = 10
+        } else if (head.x > 234 && wayPart == 10) {
+            moveSnake5(0.37, -1);
+            wayPart = 11
+        } else if (head.x > 307 && wayPart == 11) {
+            moveSnake5(0, 1);
+            wayPart = 12
+        } else if (head.y > 317 && wayPart == 12) {
+            moveSnake5(1, 0);
+            wayPart = 13
+        } else if (head.x >= 395 && wayPart == 13) {
+            moveSnake5(0, -1);
+            wayPart = 1
+        } else {
+            moveSnake5(dx, dy);
+        }
+
+    }, 1);
+}
+
+function keyPress(e) {
+
+    if (snakeCoord[0].y - length_width !== snakeCoord[1].y) {
+        moveSnake(0, -length_width);
+    }
+    if (snakeCoord2[0].y - length_width !== snakeCoord2[1].y) {
+        moveSnake2(0, -1);
+    }
+    if (snakeCoord3[0].y - length_width !== snakeCoord3[1].y) {
+        moveSnake3(0, -length_width);
+    }
+    if (snakeCoord4[0].y - length_width !== snakeCoord4[1].y) {
+        moveSnake4(0, -1);
+    }
+    if (snakeCoord5[0].y - length_width !== snakeCoord5[1].y) {
+        moveSnake5(0, -1);
+    }
+}
+
+
+drawSnake();
+keyPress();
 $('.modal-call').on('click', function (e) {
     e.preventDefault();
     $('.overlay-call').addClass('overlay-active');
@@ -70,7 +338,7 @@ $(".main").slick({
     slidesToScroll: 1,
     arrows: true,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 225000,
     prevArrow: ".main_left",
     nextArrow: ".main_right"
 });
